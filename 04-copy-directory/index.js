468be1faaa9be4ef27e8885172f172cb3c760289
kseniaagapitova <kagapitova@inbox.ru>
2023-05-06 17:45:23 +0300

    04 task is ready

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs');
+const path = require('path');
 
+function changeCopyHandler(directory) {
+    let startDirectory = path.join(__dirname, directory);
+    let copyDirectory = path.join(__dirname, directory + '-copy');
+    fs.mkdir(copyDirectory, (err) => {
+        if (err && err.code === 'EEXIST') {
+            console.log('file-copy is created already!');
+        } else {
+            console.log('file-copy is created')
+        }
+        fs.readdir(copyDirectory, {withFileTypes: true}, (err, files) => {
+            if (err) {
+                console.log(err.message)
+            }
+            files.forEach(file => {
+                fs.unlink(path.join(copyDirectory, file.name), err => {
+                    if (err) {
+                        console.log(err.message)
+                    }
+                })
+            })
+            fs.readdir(startDirectory, {withFileTypes: true}, (err, files) => {
+                files.forEach((file) => {
+                    fs.copyFile(path.join(startDirectory, file.name), path.join(copyDirectory, file.name), (err) => {
+                        if (err) {
+                            console.log(err.message)
+                        }
+                    })
+                })
+                if (err) {
+                    console.log(err.message)
+                }
+            })
+        })
+    })
+}
+
+changeCopyHandler('files');
